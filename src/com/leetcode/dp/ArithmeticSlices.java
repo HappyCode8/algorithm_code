764b52a894c76241e99c866e12e4262679580f52
wangyingjie09 <2594003600@qq.com>
2023-02-20 11:48:14 +0800

    commit

--- src/com/leetcode/dp/ArithmeticSlices.java
+++ src/com/leetcode/dp/ArithmeticSlices.java
@@ -3,12 +3,14 @@ package com.leetcode.dp;
 public class ArithmeticSlices {
 	public int numberOfArithmeticSlices(int[] A){
 		int cur=0,sum=0;
-		for(int i=2;i<A.length;i++)
+		for(int i=2;i<A.length;i++) {
 			if(A[i]-A[i-1]==A[i-1]-A[i-2]){
 				cur++;
 				sum+=cur;
-			}else
+			}else {
 				cur=0;
+			}
+		}
 		return sum;
 	}
 }
